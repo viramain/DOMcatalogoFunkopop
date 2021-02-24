e922e8a3ec2f3071125f5b537e7a8504a80111b8
Valentina Iramain <valentinairamain2020@gmail.com>
2021-02-24 11:34:19 -0300

    Se agrega funcion de agregar funkopop a un arreglo

---
+++ js/admin.js
@@ -0,0 +1,26 @@
+import { Funko } from "./funkoClass.js"
+
+// arreglo global para guardar los objetos
+let listaFunkopop = [];
+
+// para que la funcion sea accedible desde el html con la llamada desde el form onsubmit="agregarFunkopop(event)
+//se agrega WINDOW
+//------ sino, usar addeventlistener desde js
+window.agregarFunkopop = function(event) {
+    // el objetivo de la funcion es agreegar un funkopop nuevo en localStorsge
+    event.preventDefault();
+    console.log("estamos dentro de la funcion agregar funkopop");
+    // traer los valores del formulario ya validados (despues de validar general)
+    let codigo = document.getElementById("codigo").value;
+    let nombre = document.getElementById("nombre").value;
+    let numSerie = document.getElementById("numSerie").value;
+    let categoria = document.getElementById("categoria").value;
+    let descripcion = document.getElementById("descripcion").value;
+    let imagen = document.getElementById("imagen").value;
+
+    let nuevoFunkopop = new Funko(codigo, nombre, numSerie, categoria, descripcion, imagen);
+    listaFunkopop.push(nuevoFunkopop);
+    console.log(listaFunkopop);
+
+
+}
---
+++ js/funkoClass.js
@@ -0,0 +1,16 @@
+export class Funko {
+    constructor(codigo, nombre, numSerie, categoria, descripcion, imagen) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.numSerie = numSerie;
+        this.categoria = categoria;
+        this.descripcion = descripcion;
+        this.imagen = imagen;
+    }
+
+    // agregar getter y setter
+}
+
+// export function saludar(){
+
+// }
